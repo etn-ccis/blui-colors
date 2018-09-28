45f2f762719944bb49ac502a37f19f6da01131cb
NASA\E9939461 <josephpboyle@eaton.com>
2018-09-27 20:28:28 -0400

    update branding

---
+++ branding/palette.js
@@ -0,0 +1,10 @@
+export const teal  = {500: '#00b2a9'};
+export const citron = { 500: '#bcc92d' };
+export const pine = { 500: '#2e6875' };
+export const emerald = { 500: '#408b35' };
+export const wine = { 500: '#9c0e33' };
+export const crimson = { 500: '#da3227' };
+export const sunset = { 500: '#eb8f2b' };
+export const rust = { 500: '#b94700' };
+export const navy = { 500: '#004672' };
+export const sky = { 500: '#87c6d4' };
