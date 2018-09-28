d16c53cdb79f689ba2859bbd4472c2f3c0c644a8
shivanipitta <31552291+shivanipitta@users.noreply.github.com>
2018-09-28 14:18:30 -0400

    Update palette.js

--- ui/src/palette.js
+++ ui/src/palette.js
@@ -1,3 +1,11 @@
+/**
+Copyright (c) 2018-present, Eaton
+ 
+All rights reserved.
+ 
+This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+**/
+
 export const blue = {
     50: '#e0eff8',
     100: '#b3d7ec',
