4b662123618d31d0d822f62545f4027c8ba9aa89
NASA\E9939461 <josephpboyle@eaton.com>
2018-09-27 22:11:28 -0400

    typo

--- branding/index.js
+++ branding/index.js
@@ -3,14 +3,14 @@ import * as Colors from './palette.js';
 export * from './palette.js';
 
 export const chartColors = [ 
-    Colors.teal,
-    Colors.citron,
-    Colors.pine,
-    Colors.emerald,
-    Colros.wine,
-    Colors.crimson,
-    Colors.sunset,
-    Colors.rust,
-    Colors.navy,
-    Colors.sky
+    Colors.teal[500],
+    Colors.citron[500],
+    Colors.pine[500],
+    Colors.emerald[500],
+    Colors.wine[500],
+    Colors.crimson[500],
+    Colors.sunset[500],
+    Colors.rust[500],
+    Colors.navy[500],
+    Colors.sky[500]
 ];
