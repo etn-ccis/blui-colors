03ca359d8e04be5e6be78c64cd2b322954c55617
shivanipitta <31552291+shivanipitta@users.noreply.github.com>
2018-09-28 14:19:37 -0400

    Update palette.js

--- branding/src/palette.js
+++ branding/src/palette.js
@@ -1,3 +1,11 @@
+/**
+Copyright (c) 2018-present, Eaton
+ 
+All rights reserved.
+ 
+This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+**/
+
 export const teal  = {500: '#00b2a9'};
 export const citron = { 500: '#bcc92d' };
 export const pine = { 500: '#2e6875' };
