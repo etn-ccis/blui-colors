5227be0ad0ac9874651704e59668e694fe7bf070
NASA\E9939461 <josephpboyle@eaton.com>
2018-09-27 21:16:33 -0400

    add chart export

--- branding/index.js
+++ branding/index.js
@@ -1,1 +1,16 @@
+import * as Colors from './palette.js';
+
 export * from './palette.js';
+
+export const chartColors = [ 
+    Colors.teal,
+    Colors.citron,
+    Colors.pine,
+    Colors.emerald,
+    Colros.wine,
+    Colors.crimson,
+    Colors.sunset,
+    Colors.rust,
+    Colors.navy,
+    Colors.sky
+];
