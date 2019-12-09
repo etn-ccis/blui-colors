f2b80dd7faac5a3cecc021461ee8e1b19fe45fb4
huayunh <hhuayun0828@gmail.com>
2019-12-09 15:19:24 -0500

    update build command to tsc

--- branding/src/palette.ts
+++ branding/src/palette.ts
@@ -6,7 +6,18 @@ All rights reserved.
 This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
 **/
 
-import { BrandingColorType } from '../../colorTypes'
+type BrandingColorType = {
+    50: string,
+    100: string,
+    200: string,
+    300: string,
+    400: string,
+    500: string,
+    600: string,
+    700: string,
+    800: string,
+    900: string,
+}
 
 export const blue : BrandingColorType = {
     50: '#e0ecf5',
