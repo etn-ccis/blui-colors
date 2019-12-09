4d574a8aea3956962839c9b48e28c6f912b37784
huayunh <hhuayun0828@gmail.com>
2019-12-09 14:56:49 -0500

    add comments and license info

--- colorTypes.ts
+++ colorTypes.ts
@@ -1,3 +1,12 @@
+/**
+Copyright (c) 2018-present, Eaton
+ 
+All rights reserved.
+ 
+This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+**/
+
+// used by colors in './branding'
 export type BrandingColorType = {
     50: string,
     100: string,
@@ -11,6 +20,7 @@ export type BrandingColorType = {
     900: string,
 }
 
+// used by colors in './ui'
 export type UIColorType = BrandingColorType & {
     A100?: string,
     A200?: string,
