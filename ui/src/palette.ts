d97724910398ed9d340f4f8f9d2cb0fe2391d8e9
huayunh <hhuayun0828@gmail.com>
2019-12-11 11:08:23 -0500

    update buildTests; adjust hierarchy

--- ui/src/palette.ts
+++ ui/src/palette.ts
@@ -6,7 +6,7 @@ All rights reserved.
 This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
 **/
 
-import { PXBlueColor } from '../../colorTypes'
+import { PXBlueColor } from '../../types/colorType'
 
 export const blue : PXBlueColor = {
     50: '#e0eff8',
