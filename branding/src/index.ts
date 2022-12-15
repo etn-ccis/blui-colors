6d38bc26a437c3d606e6bad7d09be4ab9398d6aa
emclaug2 <evan.w.mclaughlin@gmail.com>
2022-12-15 08:11:18 -0500

    Revert this file

--- branding/src/index.ts
+++ branding/src/index.ts
@@ -1,9 +1,22 @@
-const people = [{name: 'test', age: 20}, {name: 'test1', age: 30}];
+/**
+ Copyright (c) 2018-present, Eaton
 
-function findPersonByName(name) {
-    people.forEach((person, index) => {
-        if (person.name === name) {
-            return people[index]
-        }
-    })
-}
+ All rights reserved.
+
+ This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+ **/
+
+export * from './palette';
+import * as Colors from './palette';
+export const chartColors = [
+    Colors.teal[500],
+    Colors.citron[500],
+    Colors.pine[500],
+    Colors.emerald[500],
+    Colors.wine[500],
+    Colors.crimson[500],
+    Colors.sunset[500],
+    Colors.rust[500],
+    Colors.navy[500],
+    Colors.sky[500],
+];
